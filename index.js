1c7880e712a947471030897892be42c09761d748
Samantha Ostrowski <isphinxs@hotmail.com>
2021-06-09 19:42:17 -0400

    commit tree 30250026650ec40e76ccc2c23c79841a6670855f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+const map = (sourceArr, func) => {
+    const newArr = [];
+    for (let i = 0; i < sourceArr.length; i++) {
+        newArr.push(func(sourceArr[i]));
+    }
+    return newArr;
+}
+
+const reduce = (sourceArr, func, startingValue) => {
+    let memo = startingValue;
+    for (let i = 0; i < sourceArr.length; i++) {
+        if (i === 0 && !memo) {
+            memo = sourceArr[i];
+        } else {
+            memo = func(sourceArr[i], memo);
+        }
+    }
+    return memo;
+}
